b7107319923135dde4da9bbafea5ea3faf2aa6d3
sam <sgsparks@gmail.com>
2024-04-26 12:26:02 -0700

    BASIC SERVER

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
+
+const todos = [
+  {id: 1, text: 'words'},
+  {id: 2, text: 'word'},
+  {id: 3, text: 'wor'},
+  {id: 4, text: 'wo'}
+]
 const server = http.createServer((req, res)=> {
-  console.log(req);
-  res.end();
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('X-Powered-By', 'Node.js')
+  res.end(JSON.stringify({
+    success: true,
+    data: todos
+  }));
 })
 
 const PORT = 6000;
